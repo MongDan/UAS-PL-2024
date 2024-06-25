5a8a43b6d655bbf8e670d1db031bcd3f438ca478
Dandy Teguh Pratama <145645934+MongDan@users.noreply.github.com>
2024-06-25 11:53:41 +0700

    Update Trapezoid.java

--- Trapezoid.java
+++ Trapezoid.java
@@ -5,36 +5,46 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Trapezoid {
+public class Trapezoid2 {
     public static void main(String[] args) {
-    String namafile = "C:\\Users\\MONG\\IdeaProjects\\UASPL\\src\\Trapesium.txt";
-    String outputfile = "C:\\Users\\MONG\\IdeaProjects\\UASPL\\src\\KelilingBlmDiurut.txt";
-        List<Luas> tr = new ArrayList<>();
-
-        try{
-            BufferedReader nr = new BufferedReader(new FileReader(namafile));
+        String nama = "C:\\Users\\MONG\\IdeaProjects\\UASPL\\src\\KelilingBlmDiurut.txt";
+        List<Double>dandy = new ArrayList<>();
+        try(BufferedReader br = new BufferedReader(new FileReader(nama))){
             String line;
-            nr.readLine();
-            while ((line = nr.readLine()) != null ){
-                String [] row = line.split(",");
-                Luas pr = new Luas(row[0],row[1],row[2]);
-                tr.add(pr);
+            while ((line = br.readLine()) != null){
+                dandy.add(Double.parseDouble(line));
             }
-        } catch (IOException e){
+        }catch (IOException e){
             System.err.println(e.getMessage());
         }
-        try{
-            FileWriter po = new FileWriter(outputfile);
-            for (Luas trt : tr){
-                double luas = trt.Hitungluas();
-                double Keliling = trt.HitungKeliling();
-                System.out.println("Luas Trapesium : " + luas);
-                po.write(Keliling + "\n");
+        String FileUrut = "C:\\Users\\MONG\\IdeaProjects\\UASPL\\src\\KelilingYangTelatDiUrut.txt";
+        String Fleblm = "C:\\Users\\MONG\\IdeaProjects\\UASPL\\src\\KelilingBlmDiurut.txt";
+        try (FileWriter dop= new FileWriter(Fleblm)){
+            for (double kelilng : dandy){
+                dop.write(kelilng + "\n");
             }
-            System.out.println("Untuk Keliling Di tulis Di File txt");
+            System.out.println("DiFile yang telah di urut");
+        }catch (IOException e){
+            System.err.println(e.getMessage());
 
-        } catch (IOException e){
+        }
+        try(FileWriter fw = new FileWriter(FileUrut)){
+            int p = dandy.size();
+            for (int i = 0; i < p - 1; i++) {
+                for (int j = 0; j < p - i - 1; j++) {
+                    if(dandy.get(j) > dandy.get(j + 1)){
+                        Double Penyimpanan = dandy.get(j);
+                        dandy.set(j, dandy.get(j + 1));
+                        dandy.set(j + 1, Penyimpanan);
+
+                    }
+                }
+            }
+            for (double kelilng : dandy){
+                fw.write(kelilng + "\n");
+            }
+        }catch (IOException e){
             System.err.println(e.getMessage());
         }
-        }
+    }
 }
